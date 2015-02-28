f83b649794c8a28b71be68a28d59d8bcc9599364
Janick <janick1992@hotmail.com>
2015-02-28 17:05:37 -0500

    Toutes les huds

--- Assets/Scripts/ObjectifScene_1.cs
+++ Assets/Scripts/ObjectifScene_1.cs
@@ -7,23 +7,42 @@ public class ObjectifScene_1 : Objectifs{
 	// Use this for initialization
 	void Start () {
 	
-		Kill.name = "Kill 10 spiders";
+		Kill.name = "Tuer 10 araignées";
 		Kill.toDo = 10;
 
-		Combos.name = "Do 5 combos in a row";
+		Combos.name = "Donnez 5 coups de suite";
 		Combos.toDo = 5;
 
-		Carry.name = "Carry 5 night light";
+		Carry.name = "Recolte 5 veilleuses";
 		Carry.toDo = 5;
 	
-		Ultime.name = "Do an Ultime Attack";
+		Ultime.name = "Faites une Attaque Ultime";
 		Ultime.toDo = 1;
 	
-		Discovert.name = "Discovert the caverne";
+		Discovert.name = "Découvrez la caverne";
 		Discovert.toDo = 1;
 
-		HighScores.name = " High Score 10 000";
+		HighScores.name = " Ayez un score de 10 000";
 		HighScores.toDo = 10000;
+
+		//Values init
+		Kill.State = false;
+		Kill.value = 100;
+		
+		Combos.State = false;
+		Combos.value = 200;
+		
+		Carry.State = false;
+		Carry.value = 500;
+		
+		Ultime.State = false;
+		Ultime.value = 200;
+		
+		Discovert.State = false;
+		Discovert.value = 250;
+		
+		HighScores.State = false;
+		HighScores.value = 1000;
 
 	}
 

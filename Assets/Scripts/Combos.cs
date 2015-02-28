fa618b5ac4e76c37181662dc59465f519b7cb291
Pascal <pasquical@msn.com>
2015-02-28 00:56:19 -0500

    Collider character

--- Assets/Scripts/Combos.cs
+++ Assets/Scripts/Combos.cs
@@ -7,7 +7,7 @@ public class Combos : MonoBehaviour {
 	private string buttonPressed;
 	private List<string> buttonList; 
 
-	//public GameObject player;
+	private BoxCollider collider;
 
 	private string combo1 = "XXX";
 	private string combo2 = "XXY";
@@ -17,31 +17,52 @@ public class Combos : MonoBehaviour {
 	private string combo6 = "YXX";
 	private string combo7 = "YBXX";
 	private string attackX = "X";
-	private string attackY;
+	private string attackY = "Y";
+	private string attackB = "B";
 
+	private float timerEndSimpleAttack;
+	private float simpleAttackTimer;
 	private float timerEndCombo;
 	private float comboTimer;
 	private float cooldownTimer;
 	private float timerEndCooldown;
 
+	private bool canCombo;
+	private bool canAttack;
 	private bool onCooldown;
 
 	void Start () 
 	{
+		collider = gameObject.GetComponent<BoxCollider>();
+		canCombo = false;
+
 		timerEndCooldown = 0.0f;
 		timerEndCombo = 0.0f;
+		timerEndSimpleAttack = 0.0f;
 		cooldownTimer = 1.0f;
 		comboTimer = 0.25f;
+		simpleAttackTimer = 0.5f;
 
 		buttonList = new List<string>();
 
 		buttonPressed = "";
 
+		canAttack = true;
 		onCooldown = false;
 	}
 
 	void Update () 
 	{
+		if(!canAttack)
+		{
+			timerEndSimpleAttack += Time.deltaTime;
+		}
+
+		if(timerEndSimpleAttack >= simpleAttackTimer)
+		{
+			canAttack = true;
+		}
+
 		if(onCooldown)
 		{
 			timerEndCooldown += Time.deltaTime;
@@ -54,6 +75,7 @@ public class Combos : MonoBehaviour {
 		else
 		{
 			timerEndCombo += Time.deltaTime;
+
 			if(timerEndCombo >= comboTimer)
 			{
 				buttonList.Clear();
@@ -64,18 +86,36 @@ public class Combos : MonoBehaviour {
 			{
 				buttonPressed = "X";
 				timerEndCombo = 0.0f;
+				if(canAttack)
+				{
+					simpleAttack("X");
+					canAttack = false;
+					timerEndSimpleAttack = 0.0f;
+				}
 			}
 
 			if(Input.GetButtonDown("Y"))
 			{
 				buttonPressed = "Y";
 				timerEndCombo = 0.0f;
+				if(canAttack)
+				{
+					simpleAttack("Y");
+					canAttack = false;
+					timerEndSimpleAttack = 0.0f;
+				}
 			}
 
 			if(Input.GetButtonDown("B"))
 			{
 				buttonPressed = "B";
 				timerEndCombo = 0.0f;
+				if(canAttack)
+				{
+					simpleAttack("B");
+					canAttack = false;
+					timerEndSimpleAttack = 0.0f;
+				}
 			}
 
 			if(buttonPressed != "")
@@ -91,28 +131,13 @@ public class Combos : MonoBehaviour {
 
 	public void addButton()
 	{
-		if(buttonList.Count == 4)
+		string finalStr = "";
+		
+		foreach(string str in buttonList)
 		{
-			string finalStr = "";
-			
-			foreach(string str in buttonList)
-			{
-				finalStr += str;
-			}
-			checkCombo(finalStr);
-			buttonList.Clear();
+			finalStr += str;
 		}
-
-		else if(buttonList.Count == 3)
-		{
-			string finalStr = "";
-
-			foreach(string str in buttonList)
-			{
-				finalStr += str;
-			}
-			checkCombo(finalStr);
-		}
+		checkCombo(finalStr);
 	}
 
 	public void checkCombo(string str)
@@ -122,7 +147,9 @@ public class Combos : MonoBehaviour {
 			buttonList.Clear();
 			onCooldown = true;
 			transform.GetComponent<CharacterAnims>().StartAttack1();
+
 			Debug.Log(combo1);
+			return;
 		}
 
 		if (str.Contains(combo2) && buttonList.Count == 3)
@@ -130,7 +157,9 @@ public class Combos : MonoBehaviour {
 			buttonList.Clear();
 			onCooldown = true;
 			transform.GetComponent<CharacterAnims>().StartAttack1();
+
 			Debug.Log(combo2);
+			return;
 		}
 
 		if (str.Contains(combo3) && buttonList.Count == 3)
@@ -138,7 +167,9 @@ public class Combos : MonoBehaviour {
 			buttonList.Clear();
 			onCooldown = true;
 			transform.GetComponent<CharacterAnims>().StartAttack1();
+
 			Debug.Log(combo3);
+			return;
 		}
 
 		if (str.Contains(combo4) && buttonList.Count == 4)
@@ -146,7 +177,9 @@ public class Combos : MonoBehaviour {
 			buttonList.Clear();
 			onCooldown = true;
 			transform.GetComponent<CharacterAnims>().StartAttack1();
+
 			Debug.Log(combo4);
+			return;
 		}
 
 		if (str.Contains(combo5) && buttonList.Count == 3)
@@ -154,7 +187,9 @@ public class Combos : MonoBehaviour {
 			buttonList.Clear();
 			onCooldown = true;
 			transform.GetComponent<CharacterAnims>().StartAttack1();
+
 			Debug.Log(combo5);
+			return;
 		}
 
 		if (str.Contains(combo6) && buttonList.Count == 3)
@@ -162,7 +197,9 @@ public class Combos : MonoBehaviour {
 			buttonList.Clear();
 			onCooldown = true;
 			transform.GetComponent<CharacterAnims>().StartAttack1();
+
 			Debug.Log(combo6);
+			return;
 		}
 
 		if (str.Contains(combo7) && buttonList.Count == 4)
@@ -170,7 +207,35 @@ public class Combos : MonoBehaviour {
 			buttonList.Clear();
 			onCooldown = true;
 			transform.GetComponent<CharacterAnims>().StartAttack1();
+
 			Debug.Log(combo7);
+			return;
 		}
+	}
+
+	public void simpleAttack(string str)
+	{
+		if(str == attackX)
+		{
+			transform.GetComponent<CharacterAnims>().StartAttack1();
+			Debug.Log(attackX);
+		}
+
+		else if(str == attackB)
+		{
+			transform.GetComponent<CharacterAnims>().StartAttack1();
+			Debug.Log(attackB);
+		}
+
+		else if(str == attackY)
+		{
+			transform.GetComponent<CharacterAnims>().StartAttack1();
+			Debug.Log(attackY);
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log("collision");
 	}
 }

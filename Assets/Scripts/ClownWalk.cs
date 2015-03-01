c0b36fd83cdded73a4130807fe2a097cb4e14004
bonome maker <broodwarsb@gmail.com>
2015-03-01 00:55:31 -0500

    anim enemies

---
+++ Assets/Scripts/ClownWalk.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClownWalk : MonoBehaviour
+{
+	bool movementSide = false;
+	float moveRotationSpeed = 400;
+	
+	void Start() 
+	{
+	
+	}
+	
+	void Update() 
+	{
+		if (GetComponent<AImob>().moving)
+		{
+			Movement();
+		}
+	}
+	
+	void Movement()
+	{
+		if (!movementSide)
+		{
+			GameObject.Find ("Rightleg").transform.RotateAround(GameObject.Find ("LowBodyClown").transform.position,
+			                                                    transform.right,
+			                                                    moveRotationSpeed * Time.deltaTime);
+			GameObject.Find ("Leftleg").transform.RotateAround(GameObject.Find ("LowBodyClown").transform.position,
+			                                                   transform.right,
+			                                                   -moveRotationSpeed * Time.deltaTime);
+		}
+		else
+		{
+			GameObject.Find ("Rightleg").transform.RotateAround(GameObject.Find ("LowBodyClown").transform.position,
+			                                                    transform.right,
+			                                                    -moveRotationSpeed * Time.deltaTime);
+			GameObject.Find ("Leftleg").transform.RotateAround(GameObject.Find ("LowBodyClown").transform.position,
+			                                                   transform.right,
+			                                                   moveRotationSpeed * Time.deltaTime);
+		}
+		
+		if (GameObject.Find ("Rightleg").transform.localRotation.eulerAngles.x <= 300 &&
+		    GameObject.Find ("Rightleg").transform.localRotation.eulerAngles.x > 270 &&
+		    !movementSide)
+		{
+			movementSide = true;
+		}
+		else if (GameObject.Find ("Rightleg").transform.localRotation.eulerAngles.x >= 60 &&
+		         GameObject.Find ("Rightleg").transform.localRotation.eulerAngles.x < 90 &&
+		         movementSide)
+		{
+			movementSide = false;
+		}
+		
+	}
+}

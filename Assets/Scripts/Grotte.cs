287433e662dc6a1aa0a24ec05807238cf562acae
JohnnyBoy <johnnyguerin@outlook.com>
2015-03-01 14:55:42 -0500

    Grotte objectif ok

---
+++ Assets/Scripts/Grotte.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grotte : MonoBehaviour {
+
+	MasterObjectifs Obj_Master;
+	// Use this for initialization
+	void Start () {
+		
+		Obj_Master = GameObject.Find("MasterObjectif").GetComponent<MasterObjectifs>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Obj_Master.discovert();
+			Destroy(gameObject);
+		}
+	}
+}
